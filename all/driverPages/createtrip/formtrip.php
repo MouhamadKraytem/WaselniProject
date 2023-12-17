ce135a69bf8e0339662baccdba711f01d1d05c38
doummar <alzahabidoummar@gmail.com>
2023-12-17 11:05:30 +0200

    color

--- all/driverPages/createtrip/formtrip.php
+++ all/driverPages/createtrip/formtrip.php
@@ -54,7 +54,7 @@ if (isset($_POST['return'])) {
     <meta charset="utf-8" />
     <title>Create Trip | NEW</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-    <link rel="stylesheet" href="forme.css" />
+    <link rel="stylesheet" href="formeee.css" />
     <link rel= " stylesheet "href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
   </head>
   <body>
