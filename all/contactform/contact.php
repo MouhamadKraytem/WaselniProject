81ebc6b9381fe3999729f1732c2297a690dca936
doummar <alzahabidoummar@gmail.com>
2023-12-17 11:24:51 +0200

    co

--- all/contactform/contact.php
+++ all/contactform/contact.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['id'])) {
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <title>Contact Form </title>
-        <link rel="stylesheet" href="contactStyle.css">
+        <link rel="stylesheet" href="contactStylee.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon>family=Material+Icons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all/min/css"/>
 
--- all/driverPages/profileDriver.php
+++ all/driverPages/profileDriver.php
@@ -46,7 +46,7 @@ $id = $_SESSION['id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Table</title>
-    <link rel="stylesheet" href="./profiles.css">
+    <link rel="stylesheet" href="./profile.css">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel= " stylesheet "href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <style>
